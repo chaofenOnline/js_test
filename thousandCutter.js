f38f006db914a00a6c174acd21308eb3db9ce688
chaofenOnline <380304468@qq.com>
2017-03-17 16:23:15 +0800

    Update thousandCutter.js

--- thousandCutter.js
+++ thousandCutter.js
@@ -3,7 +3,8 @@ fn(1234456.12) // "1,234,456.12"
 
 function fn(str){
 	var arr = str.toString().split('.'); // 切割成整数和小数
-	var zs = arr[0],xs = arr[1] ? '.'+arr[1] : '';
+	var zs = arr[0];
+	var xs = arr[1] ? '.'+arr[1] : '';
 	if(zs.length<=3)return str; 
 	var zsArr = zs.split('').reverse();// 将整数部分反序转为数组
 	var out = [];
