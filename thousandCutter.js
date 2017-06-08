25bfd0daade353d422e8a350191ee0c6809140db
chaofenOnline <380304468@qq.com>
2017-06-08 16:22:20 +0800

    千位分隔符 增加正则处理

--- thousandCutter.js
+++ thousandCutter.js
@@ -21,7 +21,6 @@ function fn(str){
 
 // 正则
 function regix(str){
-    var num = parseFloat(str).toFixed(3);//这里因为我需要两位小数所以做一个限制，你们看情况做小数位的限制
-    var s = num.substring(0, (num.length-1));//只取小数位2位
-    return s && s.toString().replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+    //这里因为我需要两位小数所以做一个限制，你们看情况做小数位的限制
+    return (str || 0) && parseFloat(str).toFixed(2).toString().replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
 }
