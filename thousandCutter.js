286c13c18d25067afc1b27438c2bf54c2b81c427
chaofenOnline <380304468@qq.com>
2019-10-15 16:08:54 +0800

    千位分隔符

--- thousandCutter.js
+++ thousandCutter.js
@@ -3,9 +3,9 @@ fn(1234456.12) // "1,234,456.12"
  
 function fn(str){
 	var arr = str.toString().split('.'); // 切割成整数和小数
-	var zs = arr[0];
-	var xs = arr[1] ? '.'+arr[1] : '';
-	if(zs.length<=3)return str; 
+	var zs = arr[0]; // 整数
+	var xs = arr[1] ? '.'+arr[1] : ''; // 拼接小数位
+	if(zs.length<=3)return str; // 小于千位直接返回
 	var zsArr = zs.split('').reverse();// 将整数部分反序转为数组
 	var out = [];
 	// 循环整数反序数组并且判断是3的倍数并且不是最后一位则加逗号
@@ -15,12 +15,12 @@ function fn(str){
 			out.push(',');
 		}
 	}
-	return out.reverse().join('') + xs; // 将反序数组再次反序回原顺序
+	return out.reverse().join('') + xs; // 将反序数组再次反序回原顺序 并拼接小数位
 	
 }
 
 // 正则
-function regix(str,dot_length){
+function regix(str,dot_length=2){
     //dot_length 为小数数量
     return (str || 0) && parseFloat(str).toFixed(dot_length).toString().replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
 }
